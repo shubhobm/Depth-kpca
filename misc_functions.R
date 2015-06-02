c1349018e8c249f7d3b8f9110ea5b96ed6b0895a
Subho Majumdar <zoom.subha@gmail.com>
2015-06-02 16:53:16 -0500

    Update misc_functions.R

--- misc_functions.R
+++ misc_functions.R
@@ -3,7 +3,7 @@
 ## Same syntax as kpca in package kernlab
 ## v1: only matrix inputs supported
 ############ START OF CLASS DEFINITION ###################
-##########################################################
+########################################################
 
 ##kpcaLocantore object
 setClass("prc", representation(pcv = "matrix",
@@ -136,7 +136,7 @@ function(x,  data = NULL, na.action = na.omit, ...)
 
 ## Matrix Interface
 setMethod("kpcaLocantore",signature(x="matrix"),
-          function(x, kernel = "rbfdot", kpar = list(sigma = 0.1), features = 0, th = 1e-4, delta=0.001, na.action = na.omit, ...)
+          function(x, kernel = "rbfdot", kpar = list(sigma = 0.1), features = 0, th = 1e-4, delta=0.001, is.depth=FALSE, na.action = na.omit, ...)
 {
   x <- na.action(x)
   x <- as.matrix(x)
@@ -156,8 +156,18 @@ setMethod("kpcaLocantore",signature(x="matrix"),
   km <- kernelMatrix(kernel,x)
   ww <- matrix(sp$w, m, m, byrow=F)
   kw <- km + t(ww) %*% km %*% ww - t(ww) %*% km - km %*% ww
-  kdsqrt <- diag( 1/sqrt(diag(kw)) )
-  km <- kdsqrt %*% kw %*% kdsqrt
+  
+# determine whether to do depth kpca
+  if(is.depth){
+	mult <- kdepth.SP(x, x, kernel)
+#	mult <- 1/mult - 1
+  }
+  else{
+	mult <- 1
+  }
+
+  kdsqrt <- mult/sqrt(diag(kw))
+  km <- outer(kdsqrt, kdsqrt) * kw
 
   ## center kernel matrix
   kc <- t(t(km - colSums(km)/m) -  rowSums(km)/m) + sum(km)/m^2
@@ -214,3 +224,57 @@ spatial.median <- function(x, kernel, delta)
     out=list(mu=mu0,ep=ep, w=w)
     out
 }
+
+kdepth.SP = function(xx, x, kernel="rbfdot", kpar = list(sigma = 0.1)){
+  n = nrow(x)
+  
+  ## initialize kernel matrix
+  if(!is(kernel,"kernel"))
+    {
+      if(is(kernel,"function")) kernel <- deparse(substitute(kernel))
+      kernel <- do.call(kernel, kpar)
+    }
+  if(!is(kernel,"kernel")) stop("kernel must inherit from class `kernel'")
+
+  km.x <- kernelMatrix(kernel, x)
+  km.xx <- kernelMatrix(kernel, xx)
+  km.xx.x <- kernelMatrix(kernel, xx, x)
+
+  # calculate depth values for rows of xx
+  nn = nrow(xx)
+  dep.vec = rep(0, nn)
+  
+  for(i in 1:nn){
+    xi = km.xx.x[i,]
+    del = sqrt(km.xx[i,i] + diag(km.x) - 2*xi)
+    z = (del==0)
+    del[z] = 0
+    del[!z] = 1/del[!z]
+    
+    km.i = km.xx[i,i] + km.x - outer(xi, xi, function(x,y) x+y)
+    dep.vec[i] = 1 - sqrt(t(del) %*% km.i %*% del) / n
+  }
+  
+  dep.vec
+}
+
+## function to generate from multivariate normal
+my.mvrnorm = function(n, mu, Sigma){
+  p = length(mu)
+  # compute square root of covariance matrix
+  eo=eigen(Sigma, symmetric=TRUE)
+  sigma.sqrt=eo$vec%*%diag(eo$val^0.5)%*%t(eo$vec)
+  
+  # generate random normals from runif by box-muller transform
+  rnorm.vec = sqrt(-2*log(runif(n*p)))*cos(2*pi*runif(n*p))
+  
+  # generate sample matrix
+  sample.matrix = matrix(rep(mu, n), nrow=n, byrow=T) +
+    matrix(rnorm.vec, nrow=n, ncol=p)%*%sigma.sqrt
+  return(sample.matrix)
+}
+
+ones = function(m,n){
+  matrix(1, nrow=m, ncol=n)
+}
+
