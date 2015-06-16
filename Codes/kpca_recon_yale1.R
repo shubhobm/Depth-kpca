bfb8c76f850869cf10df6dcd8c47db44c58515bb
Subho Majumdar <zoom.subha@gmail.com>
2015-06-16 17:07:54 -0500

    Update kpca_recon_yale1.R

--- Codes/kpca_recon_yale1.R
+++ Codes/kpca_recon_yale1.R
@@ -129,3 +129,104 @@ for(nf in 2*(1:5)){
 	plot(imagematrix(sm))
 }
 par(defaultPar)
+
+###### plot all results for one some image sample
+### Reconstruction by Kwok and Tsan method
+### img.mat = denoising, img.mat0 = basis retrieval
+defaultPar = par()
+par(mfcol=c(3,7), mai=rep(0,4))
+for(nf in 1:5){
+	s = reconKwok(mod.kpc[[nf]], img.mat[i:(i+1),], k=5)
+	sm = matrix(s[1,], nrow=a, byrow=F)
+	plot(imagematrix(sm))
+
+	s = reconKwok(mod.kpcLoc[[nf]], img.mat[i:(i+1),], k=5)
+	sm = matrix(s[1,], nrow=a, byrow=F)
+	plot(imagematrix(sm))
+
+	s = reconKwok(mod.kpcDep[[nf]], img.mat[i:(i+1),], k=5)
+	sm = matrix(s[1,], nrow=a, byrow=F)
+	plot(imagematrix(sm))
+}
+sm = matrix(img.mat0[i,], nrow=a, byrow=F)
+plot(imagematrix(sm))
+plot(imagematrix(sm))
+plot(imagematrix(sm))
+
+sm = matrix(img.mat[i,], nrow=a, byrow=F)
+plot(imagematrix(sm))
+plot(imagematrix(sm))
+plot(imagematrix(sm))
+par(defaultPar)
+
+## get signal-to-noise plots
+mod.kpc = list()
+mod.kpcLoc = list()
+mod.kpcDep = list()
+for(nf in 1:20){
+	mod.kpc[[nf]] <- kpca(img.mat, features=nf, kernel=k)
+	mod.kpcLoc[[nf]] <- kpcaLocantore(img.mat, features=nf, kernel=k)
+	mod.kpcDep[[nf]] <- kpcaLocantore(img.mat, features=nf, is.depth=TRUE, kernel=k)
+}
+
+SNR.list = list()
+nc = c(7,27,47)
+for(i in 1:length(nc)){
+	ni = nc[i]
+	imat = matrix(0,20,3)
+
+	for(nf in 1:20){
+		s = reconKwok(mod.kpc[[nf]], img.mat[ni:(ni+1),], k=5)
+		sd = s - img.mat0[ni:(ni+1),]
+		imat[nf,1] = mean(s[1,])/sd(sd[1,])
+
+		s = reconKwok(mod.kpcLoc[[nf]], img.mat[ni:(ni+1),], k=5)
+		sd = s - img.mat0[ni:(ni+1),]
+		imat[nf,2] = mean(s[1,])/sd(sd[1,])
+
+		s = reconKwok(mod.kpcDep[[nf]], img.mat[ni:(ni+1),], k=5)
+		sd = s - img.mat0[ni:(ni+1),]
+		imat[nf,3] = mean(s[1,])/sd(sd[1,])
+	}
+	SNR.list[[i]] = imat
+}
+
+lapply(nc, function(i) mean(img.mat0[nc[i],])/sd(img.mat0[nc[i],]-img.mat[nc[i],]))
+
+par(mfrow=c(1,3))
+for(i in 1:length(nc)){
+	plot(SNR.list[[i]][,1], type="b", ylim=c(0,max(SNR.list[[i]]+0.5)), lwd=2, lty=3, pch=2)
+	lines(SNR.list[[i]][,2], type="b", lty=2, lwd=2, pch=1)
+	lines(SNR.list[[i]][,3], type="b", lty=1, lwd=2, pch=19)
+	abline(h=mean(img.mat0[nc[i],])/sd(img.mat0[nc[i],]-img.mat[nc[i],]), lty=4)
+}
+par(mfrow=c(1,1))
+
+## noise histogram plots and noise mean/sd table
+nf.list=2*(1:5)
+SNR.list1 = list()
+par(mfrow=c(3,5), mai=rep(0.1,4))
+for(i in 1:length(nc)){
+	ni = nc[i]
+	imat = matrix(0,5,6)
+	for(j in 1:5){
+		nf=nf.list[j]
+		s = reconKwok(mod.kpcDep[[nf]], img.mat[ni:(ni+1),], k=5)
+		sd = s - img.mat0[ni:(ni+1),]
+		plot(density(sd), lty=1, lwd=2, xlim=c(-1,1), ylim=c(0,5), main="", xlab="")
+		imat[nf/2,1:2] = c(mean(sd[1,])/sd(sd[1,]), mean(s[1,])/sd(sd[1,]))
+
+		s = reconKwok(mod.kpcLoc[[nf]], img.mat[ni:(ni+1),], k=5)
+		sd = s - img.mat0[ni:(ni+1),]
+		lines(density(sd), lty=2, lwd=2)
+		imat[nf/2,3:4] = c(mean(sd[1,])/sd(sd[1,]), mean(s[1,])/sd(sd[1,]))
+
+		s = reconKwok(mod.kpc[[nf]], img.mat[ni:(ni+1),], k=5)
+		sd = s - img.mat0[ni:(ni+1),]
+		lines(density(sd), lty=3, lwd=2)
+		imat[nf/2,5:6] = c(mean(sd[1,])/sd(sd[1,]), mean(s[1,])/sd(sd[1,]))
+	}
+	SNR.list1[[i]] = imat
+}
+par(defaultPar)
+SNR.list1[[1]][,c(2,4,6)]
