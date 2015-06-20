5ec89cb8c8eda704167b81d43befd2037f361533
malli066 <malli066@umn.edu>
2015-06-20 12:16:39 -0400

    Simulation 1

--- Codes/simnoise.R
+++ Codes/simnoise.R
@@ -9,6 +9,18 @@ sigma <- 1
 x <- matrix(rnorm(10*(ntrain + ntest),sd=sigma),ncol=10)
 x.test <- x[-(1:100),]
 x.train <- x[1:100,]
+
+#Add noise to Training data
+
+#Gaussian noise
+x.train.g <- x.train + matrix(rnorm(10*ntrain,sd=sigma/10),ncol=10)
+#salt & Pepper noise
+p <- 0.1
+noiseindex <- sample.int(10*ntrain,floor(10*ntrain*p/2))
+tmp <- as.numeric(x.train)
+tmp[noiseindex] <- -sign(tmp[noiseindex])*tmp[noiseindex]
+x.train.s <- matrix(tmp,nrow=ntrain,ncol = 10)
+
 #PCA
 res <- prcomp(x.train, center = TRUE, scale = FALSE)
 test.res <- predict(res,x.test)
@@ -32,9 +44,9 @@ s_kpc.Kw = reconKwok(mod.kpc, x.test, k=5)
 s_kpcLoc.Kw <- reconKwok(mod.kpcLoc, x.test, k=5)
 s_kpcDep.Kw <- reconKwok(mod.kpcDep, x.test, k=5)
 
-s_kpc.mika <- reconMika(mod.kpc, x.test)
-s_kpcLoc.mika <- reconMika(mod.kpcLoc, x.test)
-s_kpcDep.mika <- reconMika(mod.kpcDep, x.test)
+#s_kpc.mika <- reconMika(mod.kpc, x.test)
+#s_kpcLoc.mika <- reconMika(mod.kpcLoc, x.test)
+#s_kpcDep.mika <- reconMika(mod.kpcDep, x.test)
 
 #Find the distance
 diff0 <- norm(x.test - res.trunc,type = "F")
@@ -44,17 +56,21 @@ diff_kpc.Kw <- norm(x.test - s_kpc.Kw,type = "F")
 diff_kpcLoc.Kw <- norm(x.test - s_kpcLoc.Kw,type = "F")
 diff_kpcDep.Kw <- norm(x.test - s_kpcDep.Kw,type = "F")
 
-diff_kpc.mika <- norm(x.test - s_kpc.mika,type = "F")
-diff_kpcLoc.mika <- norm(x.test - s_kpcLoc.mika,type = "F")
-diff_kpcDep.mika <- norm(x.test - s_kpcDep.mika,type = "F")
+#diff_kpc.mika <- norm(x.test - s_kpc.mika,type = "F")
+#diff_kpcLoc.mika <- norm(x.test - s_kpcLoc.mika,type = "F")
+#diff_kpcDep.mika <- norm(x.test - s_kpcDep.mika,type = "F")
 
 result <- data.frame(kpc.Kw = diff_kpc.Kw/diff0,
                      kpcLoc.Kw = diff_kpcLoc.Kw/diff0,
-                     kpcDep.Kw = diff_kpcDep.Kw/diff0,
-                     kpc.mika = diff_kpc.mika/diff0,
-                     kpcLoc.mika = diff_kpcLoc.mika/diff0,
-                     kpcDep.mika = diff_kpcDep.mika/diff0
+                     kpcDep.Kw = diff_kpcDep.Kw/diff0
+#                     kpc.mika = diff_kpc.mika/diff0,
+#                     kpcLoc.mika = diff_kpcLoc.mika/diff0,
+#                     kpcDep.mika = diff_kpcDep.mika/diff0
                      )
 result
+
+##PCA on Noisy Dataset:
+
+
 
 # Why is every method performing worse than linear PCA?
